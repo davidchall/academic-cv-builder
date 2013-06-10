a0f9cfee930b682a6742fa117ad0d164ac7c3c8b
David Hall <dhcrawley@gmail.com>
2013-06-10 23:04:39 +0100

    Moved to using setuptools for packaging. It has a develop command for testing project as you go.

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from distutils.core import setup
+import ez_setup
+ez_setup.use_setuptools()
 
-setup(name='academic-cv-writer',
-      version='1.0',
-      py_modules=['cv-writer']
-      requires=['PyYAML (>=3.01)', 'Jinja2', 'pybtex (>=0.16)']
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name='academic-cv-builder',
+    version='0.1',
+    packages=find_packages(),
+    scripts=['scripts/cv-builder'],
+    data_files=[('templates', ['templates/*.tex', 'templates/*.yaml'])],
+    install_requires=['PyYAML>=3.01', 'Jinja2', 'pybtex>=0.16'],
+)
